d7256e014437ff909fb9e82cfa3facdd42a5e2cb
Tomislav Gajsak <tomislav.gajsak@comsysto.com>
2015-02-04 11:31:08 +0100

    Fix project structure

--- src/test/java/com/example/respositories/UserRepositoryTest.java
+++ src/test/java/com/example/respositories/UserRepositoryTest.java
@@ -1,7 +1,5 @@
 package com.example.respositories;
 
-import com.example.Application;
-import com.example.config.FakeMongoConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +7,9 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
+
+import com.example.Application;
+import com.example.config.FakeMongoConfig;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {FakeMongoConfig.class, Application.class}, loader = AnnotationConfigContextLoader.class)
