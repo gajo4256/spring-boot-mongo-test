f76c067fd47beed7303ca045e4c3b45cc9892b08
Tomislav Gajsak <tomislav.gajsak@comsysto.com>
2015-02-04 11:47:49 +0100

    Simple test for userRepo, override hash and equals in user model.

--- src/main/java/com/example/models/User.java
+++ src/main/java/com/example/models/User.java
@@ -1,5 +1,8 @@
 package com.example.models;
 
+
+import java.util.Objects;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
@@ -42,6 +45,27 @@ public class User {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getFirstName(), getLastName());
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+
+        if (!(this.getClass().isInstance(obj))) {
+            return false;
+        }
+
+        final User user = (User) obj;
+        return Objects.equals(getId(), user.getId())
+                && Objects.equals(getFirstName(), user.getFirstName())
+                && Objects.equals(getLastName(), user.getLastName());
     }
 
     @Override
--- src/test/java/com/example/respositories/UserRepositoryTest.java
+++ src/test/java/com/example/respositories/UserRepositoryTest.java
@@ -1,29 +1,37 @@
 package com.example.respositories;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
 import com.example.Application;
 import com.example.config.FakeMongoConfig;
+import com.example.models.User;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {FakeMongoConfig.class, Application.class}, loader = AnnotationConfigContextLoader.class)
 public class UserRepositoryTest {
 
     @Autowired
-    private MongoTemplate template;
-
-    @Autowired
     private UserRepository underTest;
 
     @Test
-    public void someTest() throws Exception {
-        throw new UnsupportedOperationException("Not yet implemented");
+    public void saveRepoExampleTest() throws Exception {
+
+        final User user = new User();
+        user.setFirstName("John");
+        user.setLastName("Doe");
+
+        underTest.save(user);
+
+        assertEquals(user, underTest.findOne(user.getId()));
+
+
     }
 
 
