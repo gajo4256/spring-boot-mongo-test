90f85d1f458b6edfd46a19db6239b4d397a3968e
Tomislav Gajsak <tomislav.gajsak@comsysto.com>
2015-02-11 16:17:20 +0100

    Use @SpringBootApplication, application.properties is read by default.

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -1,14 +1,9 @@
 package com.example;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource(value = "classpath:application.properties")
 public class Application {
 
     public static void main(String[] args) {
