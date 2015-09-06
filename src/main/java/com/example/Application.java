86f184c6a36ee911a1c477f0ee9c3a73e5cb6673
Tomislav Gajsak <tomislav.gajsak@comsysto.com>
2015-09-06 22:04:01 +0200

    Use scanBasePackages for @SpringBootApplication, new to spring-boot 1.3.0.M5

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -3,7 +3,11 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+        "com.example.config",
+        "com.example.controllers",
+        "com.example.repositories",
+        "com.example.services" })
 public class Application {
 
     public static void main(String[] args) {
